66168b7b0d52e753db17297d06049a7ae1ca39a5
Makiver <illloquehaces@hotmail.com>
2025-02-25 13:40:53 +0100

    modificar component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AbautComponent } from "./abaut/abaut.component";
 import { FooterComponent } from "./footer/footer.component";
@@ -9,7 +8,7 @@ import { ProjectComponent } from './project/project.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, RouterOutlet, AbautComponent, FooterComponent, DescriptionComponent, ProjectComponent],
+  imports: [NavComponent, AbautComponent, FooterComponent, DescriptionComponent, ProjectComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
